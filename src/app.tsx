29ece0b84271a1ae237e67b41aa34f981aed1062
Shirajuki <shirajuki00@gmail.com>
2022-05-17 23:23:39 +0200

    feat: add eslint and prettier config

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,11 @@
-import "./styles/index.scss";
+import './styles/index.scss';
 
 export const App = () => {
   return (
     <>
       <p>Hello Vite + Preact!</p>
       <p>
-        aasd Learn Preact
-        <a>asd</a>
+        <a>Pomopond</a>
       </p>
     </>
   );
