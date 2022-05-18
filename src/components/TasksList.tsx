3edc62b877e5174197584cafb5a64e1a7e60d6b1
Shirajuki <shirajuki00@gmail.com>
2022-05-19 00:48:56 +0200

    feat: add TasksList styling

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,11 +1,23 @@
 import { h } from 'preact';
 import { useState } from 'preact/hooks';
 import styles from '../styles/taskslist.module.scss';
+import { ProfileIcon } from './icons';
 
 const TasksList = () => {
   return (
     <div class={styles.tasks}>
-      <p></p>
+      <div class={styles.taskTitle}>
+        <p>Tasks</p>
+        <div class={styles.buttons}>
+          <button>
+            <ProfileIcon />
+          </button>
+        </div>
+      </div>
+      <div class={styles.taskWrapper}>
+        <div></div>
+        <button>Add task</button>
+      </div>
     </div>
   );
 };
